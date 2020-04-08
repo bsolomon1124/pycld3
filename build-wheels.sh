d6b274c5cc6df88026fb8159de7f513ad1108d79
Brad Solomon <bsolomon@protonmail.com>
2020-04-08 12:06:20 -0400

    Fix unittest invocation; dump system info to file

--- build-wheels.sh
+++ build-wheels.sh
@@ -2,32 +2,42 @@
 
 set -ex
 
+{
+  cat /etc/redhat-release;
+  ls -UC /opt/python/
+  protoc --version
+  pkg-config --cflags protobuf
+  pkg-config --libs protobuf
+} > system_info.log
+
 for PYBIN in /opt/python/*/bin; do
-    case "$PYBIN" in 
-      *'27'*)
-        ;;
-      *)
-        # Py3 only
-        echo "Building wheel for $PYBIN"
-        "${PYBIN}/pip" install --disable-pip-version-check --upgrade -r /io/requirements-dev.txt
-        "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-        ;;
-    esac
+  case "$PYBIN" in 
+    *'27'*)
+      ;;
+    *)
+      # Py3 only
+      echo "Building wheel for $PYBIN"
+      "${PYBIN}/pip" install --disable-pip-version-check --upgrade -r /io/requirements-dev.txt
+      "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+      ;;
+  esac
 done
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
-    echo "Vendoring in external shared libs for $whl"
-    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+  echo "Vendoring in external shared libs for $whl"
+  auditwheel repair "$whl" --plat "$PLAT" -w /io/wheelhouse/
 done
 
 for PYBIN in /opt/python/*/bin/; do
-    case "$PYBIN" in 
-      *'27'*)
-        ;;
-      *)
-        echo "Installing for $PYBIN"
-        "${PYBIN}/pip" install pycld3 --disable-pip-version-check --no-index -f /io/wheelhouse
-        "${PYBIN}/pip" -m unittest
-    esac
+  case "$PYBIN" in 
+    *'27'*)
+      ;;
+    *)
+      echo "Installing for $PYBIN"
+      "${PYBIN}/pip" install pycld3 --disable-pip-version-check --no-index -f /io/wheelhouse
+      "${PYBIN}/python" -m unittest discover -v -s /io/
+  esac
 done
+
+
