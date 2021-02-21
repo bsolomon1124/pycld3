c22df256930768e1590ed65a81110fee898a5168
Brad Solomon <bsolomon@protonmail.com>
2021-02-21 00:24:47 -0500

    [meta] doc and setup updates for 3.9 support

--- setup.py
+++ setup.py
@@ -134,6 +134,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3 :: Only",
     "Programming Language :: C++",
     "Development Status :: 3 - Alpha",
